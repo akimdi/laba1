b4643d1f69dfe93951b27020ebf210de9996bd8d
akim <akimdi@gmail.com>
2016-01-25 19:13:53 +0300

    add project

---
+++ source/Program.cpp
@@ -0,0 +1,32 @@
+#include "Cars.h"
+#include <locale>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	Cars* cars = new Cars(ECarType::C_BMW);
+
+	try
+	{
+		cars->StartMotor();
+		cars->SetSpeed(150);
+		cars->SetSpeed(0);
+		cars->StopMotor();
+
+		cars->SetCar(ECarType::C_VAZ2115);
+		cars->StartMotor();
+		cars->SetSpeed(80);
+		cars->SetSpeed(0);
+		cars->StopMotor();
+	}
+	catch (exception ex)
+	{
+		cout << ex.what() << endl;
+	}
+
+	delete cars;
+
+	system("pause");
+	return 0;
+}
